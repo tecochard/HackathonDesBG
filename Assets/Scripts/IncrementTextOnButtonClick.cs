04cc4e3e70f327c4b906b20f673ccda0bf07fff8
Edouuu <fr1310edouard@gmail.com>
2023-01-26 17:30:08 +0100

    Ajout Scripts et Prefabs Enigme 1 Ajout TMPro

---
+++ Assets/Scripts/IncrementTextOnButtonClick.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class IncrementTextOnButtonClick : MonoBehaviour
+{
+    public Button incrementButton; // Assign the button in the inspector
+    public Button decrementButton;
+    public TextMeshProUGUI displayText; // Assign the text element in the inspector
+    private int currentValue = 0;
+    
+
+    private void Start()
+    {
+        incrementButton.onClick.AddListener(IncrementText);
+        decrementButton.onClick.AddListener(DecrementText);
+    }
+
+    private void IncrementText()
+    {
+        currentValue++;
+        if (currentValue == 10)
+        {
+            currentValue = 0;
+        }
+        displayText.text = currentValue.ToString();
+    }
+
+    private void DecrementText()
+    {
+        currentValue--;
+        if (currentValue == -1)
+        {
+            currentValue = 9;
+        }
+        displayText.text = currentValue.ToString();
+    }
+}
