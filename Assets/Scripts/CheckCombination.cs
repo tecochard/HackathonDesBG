13d83f39432d5ef142409ea888c581d8de1a19c4
Edouuu <fr1310edouard@gmail.com>
2023-01-27 11:32:56 +0100

    Ajout d'un prefab Salles Ajout des textures de murs Changement Script CheckCombination Changement Main Scene

--- Assets/Scripts/CheckCombination.cs
+++ Assets/Scripts/CheckCombination.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using TMPro;
 public class CheckCombination : MonoBehaviour
 {
+    public GameObject room1;
+    public GameObject room2;
+
     public TMP_Text text1;
     public TMP_Text text2;
     public TMP_Text text3;
@@ -12,8 +15,10 @@ public class CheckCombination : MonoBehaviour
     void Update()
     {
         string combination = text1.text + text2.text + text3.text + text4.text;
-        if (combination == "2345")
+        if (combination == "4592")
         {
+            room1.SetActive(false);
+            room2.SetActive(true);
             Debug.Log("Combination is correct!");
         }
     }
